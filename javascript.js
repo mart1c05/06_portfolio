8e3e697a11462ea293f659db381117927af7f893
Martha Scales <mart1c05@stud.kea.dk>
2022-01-06 09:00:28 +0100

    rettet

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// window.addEventListener("load", sidenVises);
+// function sidenVises(){  
+// // document.querySelector("#logo_knap").addEventListener("click", index.html);
+// }
+window.location.href = "http://marthascales.dk/kea/06_portfolio/index.html";
+window.location.replace = "http://marthascales.dk/kea/06_portfolio/index.html";
